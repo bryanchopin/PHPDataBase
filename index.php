5c70b388eddac5f47b84aeb05d4ddd81664107fd
brychxpin <bryan.oliveros17@tectijuana.edu.mx>
2022-03-20 20:03:31 -0700

    Test

--- index.php
+++ index.php
@@ -8,19 +8,11 @@
 @import url("css/mycss.css");
 </style>
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
+
 </head>
 <body>
 <div class="todo">
-  
-  <div id="cabecera">
-  	<img src="images/swirl.png" width="1188" id="img1">
-  </div>
+
   
   <div id="contenido">
   	<table style="margin: auto; width: 800px; border-collapse: separate; border-spacing: 10px 5px;">
@@ -49,14 +41,9 @@
         }
       ?>
 
-     
-  		
   	</table>
   </div>
-  
-	<div id="footer">
-  		<img src="images/swirl2.png" id="img2">
-  	</div>
+
 
 </div>
 
